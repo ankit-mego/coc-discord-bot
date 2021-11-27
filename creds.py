836dc5ccd3bf04accba226ab4f89c0b81de5a7cb
ankit-mego <64592422+ankit-mego@users.noreply.github.com>
2021-11-27 20:46:05 +0530

    Add files via upload

---
+++ creds.py
@@ -0,0 +1,13 @@
+# CLASH OF CLANS API
+# Your Clash of Clash Developer Account email & password will be used to auto-generate a Key,
+# so don't worry about creating a new Key
+coc_dev_email = "ankit.megotia07@gmail.com"  # Clash of Clans Developer Account email
+coc_dev_password = "g7vnCQiEgHDGQhn"  # Clash of Clans Developer Account password
+
+# DISCORD DEVELOPER
+discord_bot_token = "OTExMjYyNDMwNTI3NjMxMzkw.YZe1RQ.S1FitY9JyAhVkcw_zRnO3H6w4vo"  # 59 character Bot Token from your Discord App
+
+# OTHER INFORMATION
+clan_tag = ""  # Your clan's tag - this doesn't have to be in creds, but if you're going to use it a lot...
+default_channel = 914142555317358602  # Default Discord Channel ID for messages
+war_channel = 914142590230749235  # Discord Channel ID for war reporting
